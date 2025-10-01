4742fcd21c325f7026fa76b3cdde247c0395f255
Paul Reichmuth <p.n.reichmuth@gmail.com>
2025-10-01 08:56:27 +0200

    add core spring application from spring initializr

---
+++ src/main/java/de/pnreichmuth/timekeep_backend/TimekeepBackendApplication.java
@@ -0,0 +1,13 @@
+package de.pnreichmuth.timekeep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TimekeepBackendApplication.class, args);
+	}
+
+}
